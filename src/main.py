93b731acee8620ddad8b999856928b2b696ef438
ashu3103 <ash2003sharma@gmail.com>
2025-07-17 18:12:23 +0530

    enable logging: bootstrap setup

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import logging
+import atexit
+from typing import List
+
+def usage():
+    print('usage')
+    pass
+
+def version():
+    print('version')
+    pass
+
+def initLogger():
+    formatter = logging.Formatter(
+        fmt='%(asctime)s %(levelname)s %(filename)s:%(lineno)d %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logger = logging.getLogger('benchline')
+    logger.setLevel(logging.DEBUG)
+
+    ## Get appropriate handler
+    hndlr = logging.StreamHandler()
+    hndlr.setFormatter(formatter)
+
+    logger.addHandler(hndlr)
+    return logger
+
+logger = initLogger()
+# Register a clean logging shutdown
+atexit.register(logging.shutdown)
+
+
+
+

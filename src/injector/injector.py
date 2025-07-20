f3595883caa8902b8d444c876d708ea6b888bb8f
ashu3103 <ash2003sharma@gmail.com>
2025-07-21 00:56:21 +0530

    [#2] Injector: wrap a for loop of benchmark in a benchmark function

--- src/injector/injector.py
+++ src/injector/injector.py
@@ -15,21 +15,75 @@ import subprocess
 import logging
 from typing import TextIO
 
+logger = logging.getLogger(__name__)
+
 #### Constants Definitions ####
+
+_UT_FUNCTION_SCOPE = 0
+_UT_TEST_ARG_VARIABLE_NAME = None
 
 ## Tokens
 CURLY_BRACE_OPEN  = '{'
 CURLY_BRACE_CLOSE = '}'
+PARENTHESIS_OPEN  = '('
+PARENTHESIS_CLOSE = ')'
 SPACE             = ' '
 NEWLINE           = '\n'
+TAB               = '\t'
 
 ## Keywords
-FUNCTION_KEYWORD  = 'func'
+FUNCTION_KEYWORD          = 'func'
+UT_PARAMETER_TYPE_KEYWORD = '*testing.T'
+BT_PARAMETER_TYPE_KEYWORD = '*testing.B'
 
 #### Regex Definitions ####
 
-UT_FUNC_PATTERN      = r'func\s+Test\w*'
-UT_FUNC_NAME_PATTERN = r'Test\w*'
+FUNC_KEYWORD_DETECTION_PATTERN       = r'^func(?:\s+\S.*)?$'
+FUNC_TEST_NAME_DETECTION_PATTERN     = r'(?:^|\S\s+)Test\S+\s*\(.*$'
+FUNC_TEST_ARG_TYPE_DETECTION_PATTERN = r'\S+\s+\*testing\.T\s*[,)].*'
+FUNC_BLOCK_START_DETECTION_PATTERN   = r'.*\)\s*\{\s*'
+
+#### Test Cases ####
+test_cases_func_detect = [
+    "func",
+    "func main()",
+    "func   SomethingElse()",
+    "func\tTabbedFunc()",
+    "funcTest()",
+    "x func y",
+]
+
+test_cases_func_name_detection = [
+    "TestX(",
+    "abc   TestX(",
+    "func   TestSomething(",
+    "func TestAbc   (",
+    "func TestXyz\t (",
+    "somePrefix TestDoSomething  (abc)",
+    "Test (",
+    "   TestX(",
+    "func Test ("
+]
+
+test_cases_test_arg_detection = [
+    "t *testing.T)",
+    "myVar     *testing.T  )",
+    "foo\t\t*testing.T, something else",
+    "abc *testing.T  , trailing text",
+    "*testing.T)",
+    "   *testing.T)",
+    "var*testing.T)",
+]
+
+test_cases_func_block_start = [
+    ") }",
+"func() )   {",
+    "foo)     {",
+    ")    {   ",
+    "){",
+    "){ ) }",
+    ")   { // comment"
+]
 
 #### Class Definitions ####
 
@@ -57,55 +111,134 @@ class Peekable:
             return self._peeked
         return next(self._iterator)
 
-#### Function Definitions ####    
 
-logger = logging.getLogger(__name__)
+#### Function Definitions ####
+
+def _isValidLine(line: str) -> bool:
+    if (line == ''): return False
+    return True
+
+def _extractVariableName(line: str) -> str:
+    l = line.strip()
+    v = ""
+
+    if line.count("("):
+        v = l.split('(')[1]
+    else:
+        v = l
+    return v.strip().split(',')[0].strip().split(' ')[0]
+
+def _handleFunctionBlockStartDetection(line: str, reader: TextIO, writer: TextIO) -> bool:
+    global _UT_FUNCTION_SCOPE
+    global _UT_TEST_ARG_VARIABLE_NAME
+
+    if (re.search(FUNC_BLOCK_START_DETECTION_PATTERN, line)):
+        writer.write(line + NEWLINE)
+        _UT_FUNCTION_SCOPE = _UT_FUNCTION_SCOPE + 1
+        return True
+    else:
+        next_line = next(reader)
+        writer.write(line + NEWLINE)
+
+        processed_next_line = next_line[:].strip()
+        ## find next valid line
+        while(not _isValidLine(processed_next_line)):
+            next_line = next(reader)
+            processed_next_line = next_line[:].strip()
+        
+        if (re.search(FUNC_BLOCK_START_DETECTION_PATTERN, processed_next_line)):
+            writer.write(processed_next_line)
+            _UT_FUNCTION_SCOPE = _UT_FUNCTION_SCOPE + 1
+            return True
+        
+    _UT_TEST_ARG_VARIABLE_NAME = None
+    return False
+
+def _handleTestParameterDetection(line: str, reader: TextIO, writer: TextIO) -> bool:
+    global _UT_TEST_ARG_VARIABLE_NAME
+    ## Check if the current line has '*testing.T' if not get to next line
+    if (re.search(FUNC_TEST_ARG_TYPE_DETECTION_PATTERN, line)):
+        line = line.replace("*testing.T", "*testing.B", 1)
+        _UT_TEST_ARG_VARIABLE_NAME = _extractVariableName(line)
+        return _handleFunctionBlockStartDetection(line, reader, writer)
+    else:
+        next_line = next(reader)
+        writer.write(line + NEWLINE)
+
+        processed_next_line = next_line[:].strip()
+        ## find next valid line
+        while(not _isValidLine(processed_next_line)):
+            next_line = next(reader)
+            processed_next_line = next_line[:].strip()
+        
+        if (re.search(FUNC_TEST_ARG_TYPE_DETECTION_PATTERN, processed_next_line)):
+            processed_next_line = processed_next_line.replace("*testing.T", "*testing.B", 1)
+            _UT_TEST_ARG_VARIABLE_NAME = _extractVariableName(processed_next_line)
+            return _handleFunctionBlockStartDetection(processed_next_line, reader, writer)
+
+    return False
+
+def _handleTestFuncNameDetection(line: str, reader: TextIO, writer: TextIO) -> bool:
+    ## Check if the current line has `TestXxx`` if not get the next valid line
+    if (re.search(FUNC_TEST_NAME_DETECTION_PATTERN, line)):
+        line = line.replace("Test", "Benchmark", 1)
+        return _handleTestParameterDetection(line, reader, writer)
+    else:
+        next_line = next(reader)
+        writer.write(line + NEWLINE)
+
+        processed_next_line = next_line[:].strip()
+        ## find next valid line
+        while(not _isValidLine(processed_next_line)):
+            next_line = next(reader)
+            processed_next_line = next_line[:].strip()
+        
+        if (re.search(FUNC_TEST_NAME_DETECTION_PATTERN, processed_next_line)):
+            processed_next_line = processed_next_line.replace("Test", "Benchmark", 1)
+            return _handleTestParameterDetection(processed_next_line, reader, writer)
+
+    return False
 
 def _detectUnitTestFunctionStatement(line: str, reader: TextIO, writer: TextIO) -> bool:
     processed_line = line[:].strip()
 
-    if (re.search(UT_FUNC_PATTERN, processed_line) and processed_line.endswith(CURLY_BRACE_OPEN)): # Handle: `func TestXxx(){`
-        processed_line = processed_line.replace("Test", "Benchmark", 1)
-        processed_line = processed_line.replace("testing.T", "testing.B")
-        writer.write(processed_line + NEWLINE)
-        return True
-    elif (processed_line.startswith(FUNCTION_KEYWORD) and processed_line.endswith(FUNCTION_KEYWORD)): # Handle: `func\nTestXxx(){`
-        next_line = next(reader)
-        writer.write(processed_line + NEWLINE)
-
-        processed_next_line = next_line[:].strip()
-
-        if (re.match(UT_FUNC_NAME_PATTERN, processed_next_line)):
-            processed_next_line = processed_next_line.replace("Test", "Benchmark", 1)
-            processed_next_line = processed_next_line.replace("testing.T", "testing.B")
-            writer.write(processed_next_line + NEWLINE)
-            return True
-        else:
-            return False
-        
+    if (re.search(FUNC_KEYWORD_DETECTION_PATTERN, processed_line)):
+        return _handleTestFuncNameDetection(processed_line, reader, writer)
     return False
 
-def _doProcessFile(reader: TextIO, writer: TextIO) -> int:
-    offset = 0
-    lineno = 1
+# TODO: If the function like `_detectUnitTestFunctionStatement` can read next line, the lineno may not be
+# accuarte
+def _injectBenchmarkCode(reader: TextIO, writer: TextIO) -> int:
+    global _UT_TEST_ARG_VARIABLE_NAME
+    global _UT_FUNCTION_SCOPE
 
     try:
         while True:
             line = next(reader)
             ## Scan till the unit test (TestXxx) function is encountered
             if (_detectUnitTestFunctionStatement(line, reader, writer)):
-                logger.debug(f'unit test function detected at line: {lineno}')
+                logger.debug(f'unit test function detected')
+                ## variable name must be present
+                if not _UT_TEST_ARG_VARIABLE_NAME: 
+                    return 1
+                writer.write(f'\tfor i := 0; i < {_UT_TEST_ARG_VARIABLE_NAME}.N; i++ {CURLY_BRACE_OPEN}\n')
             else:
-                writer.write(line)
-            
-            lineno = lineno + 1
-            offset = offset + len(line)
+                if _UT_TEST_ARG_VARIABLE_NAME:
+                    _UT_FUNCTION_SCOPE = _UT_FUNCTION_SCOPE + line.count(CURLY_BRACE_OPEN) - line.count(CURLY_BRACE_CLOSE)
+
+                ## This marks the ending of a unit test function
+                if (_UT_TEST_ARG_VARIABLE_NAME and _UT_FUNCTION_SCOPE == 0):
+                    writer.write(f'\t{CURLY_BRACE_CLOSE}\n')
+                    _UT_TEST_ARG_VARIABLE_NAME = None
+
+                if (_UT_TEST_ARG_VARIABLE_NAME):
+                    ## TODO: Skip some statements which are not relevnt in benchmark tests
+                    writer.write(f'\t\t{line}')
+                else:
+                    writer.write(line)
+
     except StopIteration:
         pass
-
-
-def _injectBenchmarkCode():
-    pass
 
 ## Check the syntax of the go file using syntax checker binary
 def _checkSyntax(path: str, gosyntax_bin: str) -> bool:
@@ -127,7 +260,7 @@ def processFile(file_path: str, gosyntax_bin: str) -> int:
     """
     try:
         with open(file_path, 'r', encoding="utf-8") as r, open(temp_path, 'w', encoding="utf-8") as w:
-            return _doProcessFile(r, w)
+            return _injectBenchmarkCode(r, w)
     except FileNotFoundError:
         print(f'File {file_path} not found')
         return 1
@@ -160,7 +293,6 @@ def processFile(file_path: str, gosyntax_bin: str) -> int:
     #         return 1
 
     return 0
-
 
 if processFile("/home/ashu3103/Desktop/benchline/src/injector/tests/t0_test.go", ""):
     print("error")
--- src/injector/tests/t0_test.go
+++ src/injector/tests/t0_test.go
@@ -5,8 +5,45 @@ import (
 	"testing"
 )
 
+// TC-1
+func
 
-func Test(t *testing.T){
+TestTC1(t *testing.T) {
 	a := 3
-	return a
+	fmt.Print(a)
+}
+
+// TC-2
+func TestTC2(t *testing.T) {
+	a := 3
+	fmt.Print(a)
+}
+
+// TC-3
+func TestTC3(t *testing.T){
+	a := 3
+	fmt.Print(a)
+}
+
+// TC-4
+func TestTC4(
+	t *testing.T) {
+	a := 3
+	fmt.Print(a)
+}
+
+// TC-5
+func TestTC5(
+	t *testing.T,
+) {
+	a := 3
+	fmt.Print(a)
+}
+
+// TC-6
+func TestTC6 (
+	t *testing.T,
+){
+	a := 3
+	fmt.Print(a)
 }

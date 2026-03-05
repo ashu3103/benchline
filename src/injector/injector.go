d2a9c2de3d2cefcae9c1bc0f56f2d7b61c471e21
ashu3103 <ash2003sharma@gmail.com>
2026-03-05 14:37:54 +0530

    added file permissions check logic so that we don't parse it if benchline can't write corresponding benchmark files

--- src/injector/injector.go
+++ src/injector/injector.go
@@ -1,19 +1,63 @@
 package injector
 
 import (
-	"os"
-	"path/filepath"
+	"fmt"
 	"go/parser"
 	"go/printer"
 	"go/token"
+	"os"
+	"path/filepath"
+	"syscall"
 )
 
 const (
 	BENCHMARK_PREFIX = "benchmark_"
+	MINIMUM_PERMISSION = 0o666
 )
+
+func checkPermissions(file string) error {
+	dir := filepath.Dir(file)
+
+	info, err := os.Stat(dir)
+	if err != nil {
+		return fmt.Errorf("cannot stat directory %s: %w", dir, err)
+	}
+	
+	perms := info.Mode().Perm()
+	currentUID := os.Getuid()
+	currentGID := os.Getgid()
+
+	dirStat := info.Sys().(*syscall.Stat_t)
+	ownerUID := int(dirStat.Uid)
+	ownerGID := int(dirStat.Gid)
+
+	var isPermitted bool
+	switch {
+	case currentUID == 0:     // root can always write
+		isPermitted = true
+	case currentUID == ownerUID:  // check owner bits
+		isPermitted = perms&0200 != 0
+	case currentGID == ownerGID:  // check group bits
+		isPermitted = perms&0020 != 0
+	default:
+		isPermitted = perms&0002 != 0
+	}
+
+	if !isPermitted {
+		return fmt.Errorf("no write permissions in %s (mode: %s)", dir, perms)
+	}
+
+	return nil
+}
 
 // parse the file
 func InjectBenchmark(file string) {
+	/* Check for file permissions */
+	err := checkPermissions(file)
+	if err != nil {
+		panic(err)
+	}
+
 	/* create AST by parsing the test source file */
 	fset := token.FileSet{}
 	node, err := parser.ParseFile(&fset, file, nil, 0)

c9aeb3a91d0e3cc0e951a404f068b5873a7f3073
ashu3103 <ash2003sharma@gmail.com>
2026-03-23 15:08:15 +0530

    fixed def-use and verdict dump to file logic

--- src/main.go
+++ src/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
-	"github/ashu3103/benchline/src/injector"
-	// "github/ashu3103/benchline/src/finder"
-	// "github/ashu3103/benchline/src/runner"
+	"github/ashu3103/benchline/src/analyzer/sroa"
+	"github/ashu3103/benchline/src/loader"
+	"github/ashu3103/benchline/src/parser"
+	"github/ashu3103/benchline/src/runner"
 )
 
 func main() {
@@ -19,14 +23,93 @@ func main() {
 	var dir string
 	fmt.Scanln(&dir)
 
-	// /* find the test files */
-	// files := finder.FindTestFiles(dir)
-	
-	/* inject the benchmark in dir */
-	injector.InjectBenchmark(dir)
-	// for _, f := range files {
-	// }
+	cfg := &loader.LoadConfig{
+		Dir: dir,
+		Patterns: []string{"./..."},
+		BenchmarkOnly: true,
+	}
 
-	/* run go test */
-	// runner.RunBecnhmarkTests(dir, "out.log")
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	currentDir = filepath.Dir(currentDir)
+	currentDir = filepath.Join(currentDir, "logs")
+
+	// ----- Load Packages ------
+	pkgs, err := loader.LoadPackages(cfg)
+	if err != nil {
+		panic(err)
+	}
+	loadPkgLog := filepath.Join(currentDir, "loaded_package.log")
+	loadLogFile, err := os.Create(loadPkgLog)
+	if err != nil {
+		panic(err)
+	}
+	loader.DumpLoadedPackages(loadLogFile, pkgs)
+	loadLogFile.Close()
+
+	// ----- Run Test ------
+	rCfg := runner.NewRunConfig(dir, "", nil, nil, "")
+	rCfg.AddGcFlag("-N", "")
+	rCfg.AddGcFlag("-l", "")
+	rCfg.AddGcFlag("-m", "")
+
+	r := runner.NewRunner(rCfg)
+	rResult := r.Test()
+
+	if rResult.Err != nil {
+		panic(rResult.Err)
+	}
+
+	// ---- Parse Escape Logs ------
+	escapeMap := parser.ParseEscapesFromString(rResult.Output)
+
+	escapeMapLog := filepath.Join(currentDir, "escape_map.log")
+	escapeLogFile, err := os.Create(escapeMapLog)
+	if err != nil {
+		panic(err)
+	}
+	parser.DumpEscapeMap(escapeLogFile, escapeMap)
+	escapeLogFile.Close()
+
+	// ---- Create Def Use Chain ------
+	for _, pkg := range pkgs {
+		// find the escaped variable list
+		canonicalPkgName := loader.CanonicalPkgName(pkg.Pkg.ID)
+		fileMap := escapeMap[canonicalPkgName]
+
+		for _, fn := range pkg.TargetFuncs {
+			rel, err := filepath.Rel(dir, fn.FileName)
+			if err != nil {
+				// error
+			}
+			escapeVars := fileMap[rel]
+
+			defUseChain := analyzer.CreateDefUseChains(fn.Decl, pkg.Info, pkg.Fset, escapeVars)
+			if defUseChain == nil || len(defUseChain.Chains) == 0 {
+				continue
+			}
+
+			pathFile := strings.ReplaceAll(strings.Split(rel, ".")[0], "/", "_")
+			canonicalPkgName = strings.ReplaceAll(canonicalPkgName, "/", "_")
+			defUseLog := filepath.Join(currentDir, fmt.Sprintf("%s_%s_%s_def_use.log", canonicalPkgName, pathFile, fn.Decl.Name.Name))
+			defUseLogFile, err := os.Create(defUseLog)
+			if err != nil {
+				panic(err)
+			}
+			verdictLog := filepath.Join(currentDir, fmt.Sprintf("%s_%s_%s_verdict.log", canonicalPkgName, pathFile, fn.Decl.Name.Name))
+			verdictLogFile, err := os.Create(verdictLog)
+			if err != nil {
+				panic(err)
+			}
+
+			analyzer.DumpDefUseChain(defUseLogFile, defUseChain)
+			verdicts := analyzer.CheckViolation(defUseChain)
+			analyzer.DumpVerdicts(verdictLogFile, verdicts)
+
+			defUseLogFile.Close()
+			verdictLogFile.Close()
+		}
+	}
 }
